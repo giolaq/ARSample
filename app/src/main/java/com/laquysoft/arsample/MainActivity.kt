c3e9c475007573b82b5a8b8fe8b838a653b67d8f
Giovanni Laquidara <glaquidara@gmail.com>
2018-10-31 20:39:37 +0000

    Add infix method to place model in frame

--- app/src/main/java/com/laquysoft/arsample/MainActivity.kt
+++ app/src/main/java/com/laquysoft/arsample/MainActivity.kt
@@ -8,15 +8,18 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.google.ar.core.Anchor
-import com.google.ar.core.HitResult
+import com.google.ar.core.Frame
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.future.await
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -55,21 +58,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun addObject(model: Uri) {
         val frame = (sceneformFragment as ArFragment).arSceneView.arFrame
-        val pt = getScreenCenter()
-        val hits: List<HitResult>
-        if (frame != null) {
-            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
-            for (hit in hits) {
-                val trackable = hit.trackable
-                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
-                    uiScope.launch {
-                        placeObject((sceneformFragment as ArFragment), hit.createAnchor(), model)
-                    }
-                    break
-
-                }
-            }
-        }
+        model placeInto frame
     }
 
     private suspend fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
@@ -104,6 +93,25 @@ class MainActivity : AppCompatActivity() {
         val vw = findViewById<View>(android.R.id.content)
         return android.graphics.Point(vw.width / 2, vw.height / 2)
     }
+
+
+    private infix fun Uri.placeInto(frame: Frame?) {
+        if (frame != null) {
+            val pt = getScreenCenter()
+            val hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    uiScope.launch {
+                        placeObject((sceneformFragment as ArFragment), hit.createAnchor(), this@placeInto)
+                    }
+                    break
+
+                }
+            }
+        }
+    }
+
 }
 
 private infix fun com.google.ar.sceneform.Scene.setTo(scene: Scene) {
